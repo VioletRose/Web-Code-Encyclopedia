6ebe85527db92dd2ccd41cd6db35e3df9aaa9332
Violet Rose <pinkamenadiane314@gmail.com>
2016-05-26 15:56:40 -0700

    Update html.php

--- html.php
+++ html.php
@@ -6,83 +6,66 @@
 	<p>
 		I will make the list of elements prettier in the future, but for right now I just wanted to get started.
 	</p>
-	<table>
-		<tr>
-			<th>
-				!
-			</th>
-			<td>
-				<a href="comment.php">
-					&lt;!--&gt;
-				</a>
-			</td>
-			<td>	
-				<a href="doctype.php">
-					&lt;!DOCTYPE&gt;
-				</a>
-			</td>
-		</tr>
-		<tr>
-			<th>
-				A
-			</th>
-			<td>
-				<a href="a.php">
-					&lt;a&gt;
-				</a>
-			</td>
-			<td>
-				<a href="abbr.php">
-					&lt;abbr&gt;
-				</a>
-			</td>
-			<td>
-				<a href="address.php">
-					&lt;address&gt;
-				</a>
-			</td>
-			<td>
-				<a href="article.php">
-					&lt;article&gt;
-				</a>
-			</td>
-			<td>
-				<a href="aside.php">
-					&lt;aside&gt;
-				</a>
-			</td>
-			<td>
-				<a href="audio.php">
-					&lt;audio&gt;
-				</a>
-			</td>
-		</tr>
-		<tr>
-			<th>
-				B
-			</th>
-			<td>
-				<a href="b.php">
-					&lt;b&gt;
-				</a>
-			</td>
-			<td>
-				<a href="base.php">
-					&lt;base&gt;
-				</a>
-			</td>
-		</tr>
-		<tr>
-			<th>
-				M
-			</th>
-			<td>
-				<a href="map.php">
-					&lt;map&gt;
-				</a>
-			</th>
-		</tr>
-	</table>
+</div>
+<div class="navPanel">
+	<div>
+		<a href="comment.php">
+			&lt;!--&gt;
+		</a>
+	</div>
+		<div>	
+			<a href="doctype.php">
+				&lt;!DOCTYPE&gt;
+			</a>
+		</div>
+	<br>
+	<div>
+		<a href="a.php">
+			&lt;a&gt;
+		</a>
+	</div>
+	<div>
+		<a href="abbr.php">
+			&lt;abbr&gt;
+		</a>
+	</div>
+	<div>
+		<a href="address.php">
+			&lt;address&gt;
+		</a>
+	</div>
+	<div>
+		<a href="article.php">
+			&lt;article&gt;
+		</a>
+	</div>
+	<div>
+		<a href="aside.php">
+			&lt;aside&gt;
+		</a>
+	</div>
+	<div>
+		<a href="audio.php">
+			&lt;audio&gt;
+		</a>
+	</div>
+	<br>
+	<div>
+		<a href="b.php">
+			&lt;b&gt;
+		</a>
+	</div>
+	<div>
+		<a href="base.php">
+			&lt;base&gt;
+		</a>
+	</div>
+	<br>
+	<div>
+		<a href="map.php">
+			&lt;map&gt;
+		</a>
+	</div>
 </div>	
 <?php
     require_once('foot.php');
