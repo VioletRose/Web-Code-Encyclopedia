07b55a324c99024e9c300e608fd3e4bb6599ba36
Violet Rose <violetrose@users.noreply.github.com>
2016-07-15 16:47:28 -0700

    Finished the CSS page's basic documentation sections
    css.php: Updated; finished populating the sections on basic CSS
    concepts.
    css/csshead.php: Updated; removed redundant links and fixed an unclosed
    <code> tag.

--- css.php
+++ css.php
@@ -6,6 +6,7 @@
 	<section>
 		<p><strong>This page is not complete yet!</strong></p>
 		<p>On every page in this section, on the left side of the screen you will find links to pages for each of the properties used in CSS, sorted categorically and alphabetically, as well as indexes of CSS selectors and standard web colors. Each page will have a brief description of the property, an example of its use, and a list of accepted values. On this page, you will also find explanations of the more basic concepts in CSS.</p>
+		<p>In HTML, some elements can't be used unless they are contained inside of other ones. But CSS code doesn't, internally, have parent/child roles, it only uses them when referring to other types of code. As such, where a property can only be used alongside of another property, I've noted this in the description. <strong>Additionally, to condense the otherwise huge amount of redundancy, shorthand properties and their related properties will all be described on the same page.</strong></p>
 	</section>
 	<section>
 		<h4>What is CSS?</h4>
@@ -17,7 +18,9 @@
 			<p>Stylesheet files do not have any of the overhead an HTML document does. They simply consist of a list of entries in this format:</p>
 			<code>
 				<i>selector</i> {
+				<br>
 				&nbsp;&nbsp;&nbsp;&nbsp;<i>property</i>: <i>value</i>;
+				<br>
 				}
 			</code>
 		</figure>
@@ -25,19 +28,19 @@
 	</section>
 	<section>
 		<h4>Selectors</h4>
-		<p></p>
+		<p>Selectors specify exactly what a set of CSS properties should apply to, and can range from something as simple as the name of an HTML element to more complex attribute filters and even things like what the user's mouse is currently hovering above, or which links they've visited already. For an exhaustive list of the possibilities, please refer to the <a href="http://www.w3schools.com/cssref/css_selectors.asp">W3Schools page</a> on the subject.</p>
 	</section>
 	<section>
 		<h4>Specificity</h4>
-		<p></p>
+		<p>In CSS, a given set of style rules can apply very broadly, from everything on the page, to only elements with a specific id set (which are supposed to be used uniquely, one per site). However, given that this is the case, it easily becomes possible for different rules to conflict with each other, and when this happens, the rule with the more specific selector is the one that will be used. A wildcard selecting every element on the site has less specificity than a rule selecting only <code>&lt;div&gt;</code> elements, which has less specificity than a rule selecting a specific class, which has less specificity than a rule selecting a specific id. The more specific selector always wins a conflict, and in cases where both selectors are equally specific, it will be the one listed last in the stylesheet. There is, however, one exception to this. A CSS property set as <code><i>property</i>: <i>value</i> !important;</code> will take priority no matter the specificity of the associated selector; the only exception is if, somewhere else, the same property is set with an additional number of <code>!important</code>s greater than the first one. <strong>In almost every case, the use of <code>!important</code> is ill-advised and you should increase specificity instead.</strong></p>
 	</section>
 	<section>
 		<h4>Inherited Properties</h4>
-		<p></p>
+		<p>Some CSS properties, once applied to an element, will automatically pass down to any elements or tags it contains, <code>background-color</code> being one example. This can be very important, especially when debugging or trying to figure out why something in particular isn't working as you intended. Try to build up an understanding of which properties auto-inherit, and remember that if they don't do this by default, and you want them to, a very large number of properties also accept <code>inherit</code> as a value.</p>
 	</section>
 	<section>
-		<h4>Useful Shorthand</h4>
-		<p></p>
+		<h4>Useful Shortcuts and Shorthand Properties</h4>
+		<p>When setting values for properties, there are a couple of simple shortcuts you can use to save space and avoid adding more properties to your stylesheet than you really need. The most frequent example of this is shorthand properties, things like <code>margin</code> which encompass the function of <code>margin-left</code>, <code>margin-right</code>, and so on, which, if you know how to use them, can allow you to set multiple margin values with one property. Specifically in the case of properties defining sides, like <code>margin</code> or <code>border</code>, keep in mind that the values are arranged clockwise - top, right, down, left. And if you only give them two values, they'll interpret that as equal sides - top/bottom, right/left. A similar shortcut applies to defining colors; you can put in the full hexadecimal value, such as <code>#FF00FF</code>, or you can condense a hex value with repeated characters, i.e. <code>#F0F</code> Keep in mind, though, that it has to contain three repeated characters, as only six and three character values will be interpreted correctly.</p>
 	</section>
 </main>	
 <?php
